fa055a38f6132c71964775cfedf13297716c28d6
saul alonso <saulete_1990@hotmail.com>
2017-03-16 22:26:08 +0100

    Creacion BBDD, Encapsulacion funciones, modificacion de datos contacto y agenda (SIN ERRORES)

--- src/main/java/agenda/BaseDatos.java
+++ src/main/java/agenda/BaseDatos.java
@@ -5,6 +5,7 @@
  */
 package agenda;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -19,27 +20,17 @@ import java.io.ObjectOutputStream;
  */
 public class BaseDatos {
 
-    private final File fichero = new File(System.getProperty("user.dir") + File.separator + "agenda"+File.separator);
-    private FileInputStream fileInputStream;
-    private ObjectInputStream objectInputStream;
-    private FileOutputStream fileOutputStream;
-    private ObjectOutputStream objectOutputStream;
 
-    void guardarEnFicheros() throws IOException {
-
+    public static void guardarFicheros(Agenda historial) throws IOException {
+        File fichero = new File(System.getProperty("user.dir") + File.separator + "agenda"+File.separator);
+        FileOutputStream f = null;
+        ObjectOutputStream salida = null;
         try {
 
-            if (!(fichero.exists())) {
-                fichero.createNewFile();
-            }
-
-            fileOutputStream = new FileOutputStream(fichero);
-            objectOutputStream = new ObjectOutputStream(fileOutputStream);
-
-            objectOutputStream.writeObject(this);
-
-            fileOutputStream.close();
-            objectInputStream.close();
+            f = new FileOutputStream(fichero);
+            salida = new ObjectOutputStream(f);
+            salida.writeObject(historial);
+            salida.close();
 
         } catch (IOException e) {
 
@@ -47,16 +38,63 @@ public class BaseDatos {
         System.out.println("Correcto!! Guardado..");
     }
 
-    protected Agenda cargarFichero() throws ClassNotFoundException {
+    public static Agenda cargarFichero() throws ClassNotFoundException, FileNotFoundException, IOException {
+        File fichero = new File(System.getProperty("user.dir") + File.separator + "agenda"+File.separator);
         Agenda agenda = new Agenda();
+        FileInputStream fileInputStream;
+        ObjectInputStream objectInputStream;
+        BufferedInputStream bufferedInputStream;
+
         try {
             if (fichero.exists()) {
 
                 fileInputStream = new FileInputStream(fichero);
-                objectInputStream = new ObjectInputStream(fileInputStream);
-
+                bufferedInputStream = new BufferedInputStream(fileInputStream);
+                objectInputStream = new ObjectInputStream(bufferedInputStream);
                 agenda = (Agenda) objectInputStream.readObject();
+                fileInputStream.close();
+                objectInputStream.close();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
 
+        }
+        return agenda;
+    }
+
+    public static void guardarFicherosSeguridad(Agenda historial) throws IOException {
+        File fichero = new File(System.getProperty("user.dir") + File.separator + "agendaSeguridad"+File.separator);
+        FileOutputStream f = null;
+        ObjectOutputStream salida = null;
+        try {
+
+            f = new FileOutputStream(fichero);
+            salida = new ObjectOutputStream(f);
+            salida.writeObject(historial);
+            salida.close();
+
+        } catch (IOException e) {
+
+        }
+        System.out.println("Correcto!! Guardado..");
+    }
+
+    public static Agenda cargarFicheroSeguridad() throws ClassNotFoundException, FileNotFoundException, IOException {
+        File fichero = new File(System.getProperty("user.dir") + File.separator + "agendaSeguridad"+File.separator);
+        Agenda agenda = new Agenda();
+        FileInputStream fileInputStream;
+        ObjectInputStream objectInputStream;
+        BufferedInputStream bufferedInputStream;
+
+        try {
+            if (fichero.exists()) {
+
+                fileInputStream = new FileInputStream(fichero);
+                bufferedInputStream = new BufferedInputStream(fileInputStream);
+                objectInputStream = new ObjectInputStream(bufferedInputStream);
+                agenda = (Agenda) objectInputStream.readObject();
                 fileInputStream.close();
                 objectInputStream.close();
             }
--- src/main/java/agenda/Contacto.java
+++ src/main/java/agenda/Contacto.java
@@ -6,34 +6,56 @@ import java.util.ArrayList;
  *
  * @author Oscar de la Cuesta - www.palentino.es
  */
-public class Contacto {
-	private String nombre;
-	private ArrayList<Integer> telefono;
+public class Contacto implements Comparable {
 
-	public Contacto() {
-		this.nombre = null;
-		this.telefono = new ArrayList<>();
-	}
+    private String nombre;
+    private ArrayList<Integer> telefono;
 
-	public Contacto(String nombre, int telefono) {
-		this.nombre = nombre;
-		this.telefono.add(telefono);
-	}
+    public Contacto() {
+        this.nombre = null;
+        this.telefono = new ArrayList<>();
+    }
 
-	public void set_nombre(String nomb) {
-		this.nombre = nomb.toUpperCase();
-	}
+    public Contacto(String nombre, Integer telefono) {
+        this.nombre = nombre;
+        this.telefono.add(telefono);
+    }
 
-	public void set_telefono(int telf) {
-		this.telefono.add(telf);
-	}
+    @Override
+    public String toString() {
+        return "Contacto{" + "nombre=" + nombre + ", telefono=" + telefono + '}';
+    }
 
-	public String getNombre() {
-		return this.nombre;
-	}
+    public void set_nombre(String nomb) {
+        this.nombre = nomb.toUpperCase();
+    }
 
-	public ArrayList<Integer> getTelefono() {
-		return telefono;
-	}
+    public void set_telefono(int telf) {
+        this.telefono.add(telf);
+    }
 
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public ArrayList<Integer> getTelefono() {
+        return telefono;
+    }
+    
+    public int compareTo(Object o) {
+        Contacto c = (Contacto) o;
+        return getNombre().compareTo(c.getNombre());
+    }
+
+    public Boolean equals(Contacto c) {
+        return (this.getNombre() == c.getNombre());
+    }
+    
+    public void eliminarNumero(Integer telefono){
+        this.getTelefono().remove(telefono);
+    }
+    
+    public void anadirNumero(Integer telefono){
+        this.getTelefono().add(telefono);
+    }
 }
