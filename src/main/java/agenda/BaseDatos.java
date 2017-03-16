9b9557aa322dacee0a96b34fca92549928d8426d
saul alonso <saulete_1990@hotmail.com>
2017-03-16 11:47:31 +0100

    Modificacion Clase BBDD, errores por cambios en los tipos de datos en la clase agenda

--- src/main/java/agenda/BaseDatos.java
+++ src/main/java/agenda/BaseDatos.java
@@ -5,10 +5,68 @@
  */
 package agenda;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 /**
  *
  * @author saulalonso
  */
 public class BaseDatos {
-    
+
+    private final File fichero = new File(System.getProperty("user.dir") + File.separator + "agenda"+File.separator);
+    private FileInputStream fileInputStream;
+    private ObjectInputStream objectInputStream;
+    private FileOutputStream fileOutputStream;
+    private ObjectOutputStream objectOutputStream;
+
+    void guardarEnFicheros() throws IOException {
+
+        try {
+
+            if (!(fichero.exists())) {
+                fichero.createNewFile();
+            }
+
+            fileOutputStream = new FileOutputStream(fichero);
+            objectOutputStream = new ObjectOutputStream(fileOutputStream);
+
+            objectOutputStream.writeObject(this);
+
+            fileOutputStream.close();
+            objectInputStream.close();
+
+        } catch (IOException e) {
+
+        }
+        System.out.println("Correcto!! Guardado..");
+    }
+
+    protected Agenda cargarFichero() throws ClassNotFoundException {
+        Agenda agenda = new Agenda();
+        try {
+            if (fichero.exists()) {
+
+                fileInputStream = new FileInputStream(fichero);
+                objectInputStream = new ObjectInputStream(fileInputStream);
+
+                agenda = (Agenda) objectInputStream.readObject();
+
+                fileInputStream.close();
+                objectInputStream.close();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+
+        }
+        return agenda;
+    }
+
 }
--- src/main/java/agenda/Contacto.java
+++ src/main/java/agenda/Contacto.java
@@ -1,20 +1,23 @@
 package agenda;
+
+import java.util.ArrayList;
+
 /**
  *
  * @author Oscar de la Cuesta - www.palentino.es
  */
 public class Contacto {
 	private String nombre;
-	private int telefono;
+	private ArrayList<Integer> telefono;
 
 	public Contacto() {
 		this.nombre = null;
-		this.telefono = 0;
+		this.telefono = new ArrayList<>();
 	}
 
 	public Contacto(String nombre, int telefono) {
 		this.nombre = nombre;
-		this.telefono = telefono;
+		this.telefono.add(telefono);
 	}
 
 	public void set_nombre(String nomb) {
@@ -22,14 +25,14 @@ public class Contacto {
 	}
 
 	public void set_telefono(int telf) {
-		this.telefono = telf;
+		this.telefono.add(telf);
 	}
 
 	public String getNombre() {
 		return this.nombre;
 	}
 
-	public int getTelefono() {
+	public ArrayList<Integer> getTelefono() {
 		return telefono;
 	}
 
