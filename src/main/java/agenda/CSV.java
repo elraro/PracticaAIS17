b8c758290e00296705d57a3bd75bd9218d13e7ff
URJCheca <alexisasi12@hotmail.com>
2017-04-11 09:19:32 +0200

    Agregada clase CSV. Modificada clase contacto para poder usarla

---
+++ src/main/java/agenda/CSV.java
@@ -0,0 +1,97 @@
+package agenda;
+
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.TreeSet;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author saulalonso
+ */
+public class CSV {
+
+    private TreeSet<Contacto> lista_contactos = new TreeSet<>();
+
+
+    public static void ExportarCSV(TreeSet<Contacto>lista_contactos) throws IOException {
+    	Iterator<Contacto> it = lista_contactos.iterator();
+    	String contenido = "";
+    	while(it.hasNext()){
+    		Contacto aux = it.next();
+    		contenido=contenido+aux.getNombre()+";";
+    		ArrayList<Long> telefonos = aux.getNumeros();//Cambiar Long por String
+    		for (int i=0;i<telefonos.size();i++){
+    			contenido=contenido+telefonos.get(i)+";";
+    		}
+    		contenido=contenido+"\n";
+    	}
+        File fichero = new File(System.getProperty("user.dir") + File.separator + "agenda.csv" + File.separator);
+        FileOutputStream f = null;
+        ObjectOutputStream salida = null;
+        try {
+            f = new FileOutputStream(fichero);
+            salida = new ObjectOutputStream(f);
+            salida.writeObject(contenido);
+            salida.close();
+
+        } catch (IOException e) {
+
+        }
+        System.out.println("Correcto!! Guardado..");
+    }
+
+    public TreeSet<Contacto> ImportarCSV() throws ClassNotFoundException, FileNotFoundException, IOException {
+        File fichero = new File(System.getProperty("user.dir") + File.separator + "agenda.csv" + File.separator);
+        String contenido;
+        FileInputStream fileInputStream;
+        ObjectInputStream objectInputStream;
+        BufferedInputStream bufferedInputStream;
+        TreeSet<Contacto> lista_contactos = new TreeSet<>();
+        try {
+            if (fichero.exists()) {
+                fileInputStream = new FileInputStream(fichero);
+                bufferedInputStream = new BufferedInputStream(fileInputStream);
+                objectInputStream = new ObjectInputStream(bufferedInputStream);
+                contenido = (String) objectInputStream.readObject();
+                fileInputStream.close();
+                objectInputStream.close();
+                String [] contenidoaux  = contenido.split(";");
+                for(int i = 0; i < contenidoaux.length; i++){
+                	String nombre = contenidoaux[i];
+                	ArrayList<String> numeros = new ArrayList<String>();
+                	while (isNumeric(contenidoaux[i+1])){
+                		i++;
+                		numeros.add(contenidoaux[i]);
+                	}
+                    Contacto contacto = new Contacto();
+                    contacto.set_nombre(nombre);
+                    contacto.set_telefonos(numeros);
+                    lista_contactos.add(contacto);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+
+        }
+        return lista_contactos;
+    }
+    
+    public static boolean isNumeric(String str) {
+        return (str.matches("[+-]?\\d*(\\.\\d+)?") && str.equals("")==false);
+    }
+}
+
--- src/main/java/agenda/Contacto.java
+++ src/main/java/agenda/Contacto.java
@@ -1,36 +1,41 @@
 package agenda;
+
+import java.util.ArrayList;
+
 /**
  *
  * @author Oscar de la Cuesta - www.palentino.es
  */
 public class Contacto {
 	private String nombre;
-	private int telefono;
+	private ArrayList<String> telefonos;
 
 	public Contacto() {
 		this.nombre = null;
-		this.telefono = 0;
+		this.telefonos = new ArrayList<String>();
 	}
 
-	public Contacto(String nombre, int telefono) {
+	public Contacto(String nombre, String telefono) {
 		this.nombre = nombre;
-		this.telefono = telefono;
+		this.telefonos.add(telefono);
 	}
 
 	public void set_nombre(String nomb) {
 		this.nombre = nomb.toUpperCase();
 	}
 
-	public void set_telefono(int telf) {
-		this.telefono = telf;
+	public void set_telefono(String telf) {
+		this.telefonos.add (telf);
 	}
-
+	public void set_telefonos(ArrayList<String>telefonos){
+    	this.telefonos = telefonos;
+    }
 	public String getNombre() {
 		return this.nombre;
 	}
 
-	public int getTelefono() {
-		return telefono;
+	public ArrayList<String> getTelefono() {
+		return telefonos;
 	}
-
+	 
 }
